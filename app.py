df4c201bdb914f65be467e6e62d2c1b08546817c
Sanjay-programming <sanjay.pothuraju.works@gmail.com>
2025-11-02 18:35:26 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
 import streamlit as st
-from crewai import Crew, Task
-# from debate_agents import hr_agent, dev_agent, manager_agent, moderator
-try:
-    from debate_agents import hr_agent, dev_agent, manager_agent, moderator
-except ModuleNotFoundError:
-    from .debate_agents import hr_agent, dev_agent, manager_agent, moderator
+from crewai import Crew, Agent, Task
+import os
+from dotenv import load_dotenv
+from langchain_litellm import ChatLiteLLM
+
+load_dotenv()
+
+llm = ChatLiteLLM(
+    model ="gemini/gemini-2.5-pro",
+    temperature = 0.5,
+    verbose = True,
+    api_key = os.getenv("GOOGLE_API_KEY")
+)
+
+
+
+hr_agent = Agent(
+    role ="HR Specialist",
+    goal ="Argue from an HR and employee welfare perspective.",
+    backstory ="An empathetic HR manager who values work-life balance.",
+    llm = llm
+)
+dev_agent = Agent(
+    role="Software Developer",
+    goal="Argue from a developer’s perspective focusing on productivity and focus.",
+    backstory="A pragmatic coder who values deep work and minimal distractions.",
+    llm=llm
+)
+manager_agent = Agent(
+    role="Project Manager",
+    goal="Argue from a management perspective focusing on deadlines and collaboration.",
+    backstory="An efficiency-driven manager who emphasizes teamwork and delivery timelines.",
+    llm=llm
+)
+moderator = Agent(
+    role="Moderator",
+    goal="Summarize the debate and extract the consensus or main points of disagreement.",
+    backstory="A neutral observer ensuring fair summarization.",
+    llm=llm
+)
+
+
+
 
 
 st.set_page_config(page_title="Agentic Debate Room", layout="centered")
